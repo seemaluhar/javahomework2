0f82eb59a49c28dfb87c0880667ed8765d3cc89d
seema <soni.seema58@gmail.com>
2022-12-04 21:00:48 +0000

    javahomework2

---
+++ Agile.java
@@ -0,0 +1,11 @@
+package ManualTesting2;
+
+public class Agile {
+    public static void main(String [] args){
+        System.out.println("Software Development models");
+        System.out.println("Agile Methodology");
+        System.out.println("Verification and Validation");
+        System.out.println("Definition of Done and Ready");
+
+    }
+}
---
+++ BDDandTDD.java
@@ -0,0 +1,12 @@
+package ManualTesting2;
+
+public class BDDandTDD {
+    public static void main(String[] args) {
+        System.out.println("What is BDD and it's benefits");
+        System.out.println("What is TDD and it's benefits");
+        System.out.println("BDD keywords");
+        System.out.println("Gherkin");
+        System.out.println("Maintenance");
+        System.out.println("Welcome");
+    }
+}
